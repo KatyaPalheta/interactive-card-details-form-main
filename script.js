8a2226ee7625a5e02e0a97cd50f38b79ad6ae342
KATYA SYLENE FONTENELLE PALHETA <109077065+KatyaPalheta@users.noreply.github.com>
2022-08-08 22:21:46 -0300

    Inicio da criação do layout
    Primeiros posicionamentos do layout
    finalização das  mascaras

--- script.js
+++ script.js
@@ -6,42 +6,41 @@ const formulario__cartao_mes = document.getElementById('formulario__cartao_mes')
 const formulario__cartao_ano = document.getElementById('formulario__cartao_ano');
 const formulario__cartao_cvc = document.getElementById('formulario__cartao_cvc');
 
-formulario__cartao.addEventListener('submit', e => {e.preventDefault();checkInputs();});
+formulario__cartao.addEventListener('submit', e => { e.preventDefault(); checkInputs(); });
 
 function checkInputs() {
-	const formulario__cartao_nome_Value = formulario__cartao_nome.value.trim();
+    const formulario__cartao_nome_Value = formulario__cartao_nome.value.trim();
     const formulario__cartao_numero_Value = formulario__cartao_numero.value.trim();
-	const formulario__cartao_mes_Value = formulario__cartao_mes.value.trim();
-	const formulario__cartao_ano_Value = formulario__cartao_ano.value.trim();
+    const formulario__cartao_mes_Value = formulario__cartao_mes.value.trim();
+    const formulario__cartao_ano_Value = formulario__cartao_ano.value.trim();
     const formulario__cartao_cvc_Value = formulario__cartao_cvc.value.trim();
 
-    if(formulario__cartao_nome_Value === '') {
+    if (formulario__cartao_nome_Value === '') {
         setErrorFor(formulario__cartao_nome, "Can't be blank");
     } else {
         setSuccessFor(formulario__cartao_nome);
     }
-    if(formulario__cartao_numero_Value === '') {
-        setErrorFor(formulario__cartao_numero, 'Wrong format, numbers only');
+    if (formulario__cartao_numero_Value === '') {
+        setErrorFor(formulario__cartao_numero, 'Wrong format, numbers only, 16 digits');
     } else {
         setSuccessFor(formulario__cartao_numero);
     }
-    if(formulario__cartao_mes_Value === '') {
+    if (formulario__cartao_mes_Value === '') {
         setErrorFor(formulario__cartao_mes, "Can't be blank");
-    }else {
+    } else {
         setSuccessFor(formulario__cartao_mes);
     }
 
-    if(formulario__cartao_ano_Value === '') {
+    if (formulario__cartao_ano_Value === '') {
         setErrorFor(formulario__cartao_ano, "Can't be blank");
     } else {
         setSuccessFor(formulario__cartao_ano);
     }
-    if(formulario__cartao_cvc_Value === '') {
+    if (formulario__cartao_cvc_Value === '') {
         setErrorFor(formulario__cartao_cvc, "Can't be blank");
     } else {
         setSuccessFor(formulario__cartao_cvc);
     }
-
 }
 function setErrorFor(input, message) {
     const formControl = input.parentElement;
@@ -60,32 +59,31 @@ function setSuccessFor(input) {
     window.location.href = "https://www.google.com/";
 }
 
-
-
 //nome da função foi dado inline no html no input que esta sendo digitado pelo usuario
-function tempo_real_nome(){
+function tempo_real_nome() {
     //joga os valores digitados no input dentro da variavel
-    tempo_real = document.getElementById('formulario__cartao_nome').value; 
+    tempo_real = document.getElementById('formulario__cartao_nome').value;
     //substitui o paragrafo com o id selecionado pela variavel
     document.getElementById('cartao__nome').innerHTML = tempo_real;
 }
 
-function tempo_real_numero(){
+function tempo_real_numero() {
     tempo_real = document.getElementById('formulario__cartao_numero').value;
-    document.getElementById('cartao__numero').innerHTML = tempo_real;
-
+    var tempo_real_replace = tempo_real.replace(/\D/g, "");
+    tempo_real_replace = tempo_real_replace.replace(/(\d{4})/g, "$1 ");
+    tempo_real_replace = tempo_real_replace.substring(0, 19);
+    document.getElementById('cartao__numero').innerHTML = tempo_real_replace;
 }
 
-function tempo_real_mes(){
+function tempo_real_mes() {
     tempo_real = document.getElementById('formulario__cartao_mes').value;
-    document.getElementById('cartao__mes').innerHTML = tempo_real;   
-
+    document.getElementById('cartao__mes').innerHTML = tempo_real;
 }
-function tempo_real_ano(){
+function tempo_real_ano() {
     tempo_real = document.getElementById('formulario__cartao_ano').value;
-    document.getElementById('cartao__ano').innerHTML = tempo_real; 
+    document.getElementById('cartao__ano').innerHTML = tempo_real;
 }
-function tempo_real_cvc(){
+function tempo_real_cvc() {
     tempo_real = document.getElementById('formulario__cartao_cvc').value;
-    document.getElementById('cartao__cvc').innerHTML = tempo_real; 
+    document.getElementById('cartao__cvc').innerHTML = tempo_real;
 }
